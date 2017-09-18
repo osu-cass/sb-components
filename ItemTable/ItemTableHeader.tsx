d43a82a6c6e4b18e752023204db60844dbcaea9d
Kevin Turkington <turkingtonkevin@gmail.com>
2017-09-18 12:43:25 -0700

    header css refactor mosting matching example.

--- ItemTable/ItemTableHeader.tsx
+++ ItemTable/ItemTableHeader.tsx
@@ -151,6 +151,7 @@ export class HeaderTable extends React.Component<Props, State> {
             <table className="item-table table mapcomponent-table">
                 <thead>
                     <tr className="primary">
+                        <th></th>
                         {this.props.columns.map(col => this.renderHeader(col))}
                     </tr>
                 </thead>
