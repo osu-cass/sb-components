03cc2550762d4178fc020a6a7b200fe74f900823
Kevin Turkington <turkingtonkevin@gmail.com>
2017-09-19 16:43:42 -0700

    Adding data to filter model.

--- Filter/AdvancedFilterModel.tsx
+++ Filter/AdvancedFilterModel.tsx
@@ -1,10 +1,14 @@
 import { selection } from './AdvancedFilter';
 import { GradeLevels } from "../Models/GradeLevels";
 
+// TODO: optimize options (look at ItemSearchDropDown)
 export const Grades:selection = {
     fieldName:"Grade",
     infoDescription:"Grade level for stuffasdasd",
     options: [
-        {label:"All",code: GradeLevels.All}
+        {label:"All", code:GradeLevels.All},
+        {label:"HighSchool", code:GradeLevels.High},
+        {label:"MiddleSchool", code:GradeLevels.Middle},
+        {label:"Elementary", code:GradeLevels.Elementary}
     ]
 };
