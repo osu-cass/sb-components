607c16ff9514b2d2cd9ee86b7c5d55701d390978
Kevin Turkington <turkingtonkevin@gmail.com>
2017-09-20 11:26:36 -0700

    onclick for filter selection implemeneted

--- Filter/AdvancedFilter.tsx
+++ Filter/AdvancedFilter.tsx
@@ -2,12 +2,10 @@ import * as React from "react";
 import * as ItemModels from '../Models/ItemModels';
 import { Selection, LabelValue } from './AdvancedFilterModel';
 
-
-
-
 interface Props {
-    onClick: () => void;
-
+    isMultiSelect: boolean;
+    onClick: (val: string|number) => void;
+    
     fieldName: string;
     infoDescription:string;
     defaultValue?: string|number;
@@ -19,31 +17,25 @@ interface State extends Selection { };
 export class AdvancedFilter extends React.Component<Props, State>{
     constructor(props: Props) {
         super(props);
-
-        // if(props.defaultValue){
-        //     this.state.selectedValue = props.defaultValue;
-        // }
-        
     }
 
     //generic filter
     render() {
-        const item = this.state;
         const tags:JSX.Element[] = [];
         
-        item.options.forEach((t, i) => tags.push(
-            <button key={i} value={t.value} onClick={this.props.onClick}>{t.label}</button>
+        this.state.options.forEach((t, i) => tags.push(
+            <button key={i} value={t.value} onClick={() => this.props.onClick(t.value)}>{t.label}</button>
         ));
 
         return (
-                <div id={(item.fieldName + "-filter").toLocaleLowerCase()} className="block-child">
+                <div id={(this.state.fieldName + "-filter").toLocaleLowerCase()} className="block-child">
                     <label>
-                        <span info-label>{item.fieldName}}</span>
+                        <span info-label>{this.state.fieldName}}</span>
 
                         {/* seperate tooltip into its own react componenet */}
                         <div className="tooltip">info
                             {/*w3schools basic tooltip*/}
-                            <span className="tooltiptext">{item.infoDescription}</span>
+                            <span className="tooltiptext">{this.state.infoDescription}</span>
                         </div>
 
                     </label>
