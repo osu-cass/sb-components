f401cc5e55a230a9e0e2120c8aa39dcd7b4f9fbc
Van Horn <vanhornd@oregonstate.edu>
2017-09-26 13:18:57 -0700

    update ItemPageTable tests

--- ItemTable/__tests__/ItemPageTable.test.tsx
+++ ItemTable/__tests__/ItemPageTable.test.tsx
@@ -1,16 +1,41 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { ItemPageTable } from '../ItemPageTable';
+import { ItemPageTable, Props } from '../ItemPageTable';
+import * as GradeLevels from '../../Models/GradeLevels';
+import * as ItemCardViewModel from '../../Models/ItemCardViewModel'
 import * as TestUtils from 'react-dom/test-utils';
-import { shallow, mount } from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
 
 describe("ItemPageTable", () => {
-    const props = {
+    const itemCards: ItemCardViewModel.ItemCardViewModel[]  = [{
+        bankKey: 1,
+        itemKey: 3,
+        title: "",
+        grade: GradeLevels.GradeLevels.All,
+        gradeLabel: "",
+        subjectCode: "",
+        subjectLabel: "",
+        claimCode: "",
+        claimLabel: "",
+        target: "",
+        interactionTypeCode: "",
+        interactionTypeLabel: "",
+        isPerformanceItem: true
+    }]
+    
+    const props: Props = {
         onRowSelection: jest.fn(),
-        itemCards: [] 
+        itemCards 
     }
 
     it("matches snapshot", () => {
         expect(shallow(<ItemPageTable {...props}/>)).toMatchSnapshot();;
     })
+
+    // it("sorts list on header click", () => {
+    //     let wrapper = mount(<ItemPageTable {...props}/>);
+    //     wrapper.find('th.item').simulate('click');
+    //     expect(wrapper.state('dirElem')).toEqual('')
+
+    // })
 })
