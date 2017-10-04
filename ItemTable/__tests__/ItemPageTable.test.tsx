6b61bcb3ca18a3c5446e1d8396194f818962901f
Van Horn <vanhornd@oregonstate.edu>
2017-10-04 15:16:52 -0700

    update snapshot tests

--- ItemTable/__tests__/ItemPageTable.test.tsx
+++ ItemTable/__tests__/ItemPageTable.test.tsx
@@ -2,14 +2,17 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { ItemPageTable, Props } from '../ItemPageTable';
 import * as GradeLevels from '../../Models/GradeLevels';
-import * as ItemCardViewModel from '../../Models/ItemCardViewModel'
+import * as ItemCardViewModel from '../../Models/ItemCardViewModel';
+import * as ApiModels from '../../Models/ApiModels';
+import * as Rubric from '../../PageTabs/Rubric';
+import * as AboutItemVM from '../../Models/AboutItemVM';
 import * as TestUtils from 'react-dom/test-utils';
 import { shallow, mount, render } from 'enzyme';
 
 describe("ItemPageTable", () => {
     const tabs = ["item", "claimAndTarget", "subject", "grade", "interactionType"]
 
-    const itemCards: ItemCardViewModel.ItemCardViewModel[]  = [{
+    const itemCards: ItemCardViewModel.ItemCardViewModel[] = [{
         bankKey: 1,
         itemKey: 3,
         title: "",
@@ -24,18 +27,36 @@ describe("ItemPageTable", () => {
         interactionTypeLabel: "",
         isPerformanceItem: true
     }]
-    
+
+    const rubrics: Rubric.Rubric[] = []
+
+    const content: AboutItemVM.AboutThisItem = {
+        rubrics,
+        itemCardViewModel: itemCards[0],
+        depthOfKnowledge: "depthOfKnowledge",
+        targetDescription: "targetDescription",
+        commonCoreStandardsDescription: "commonCoreStandardsDescription",
+        educationalDifficulty: "educationalDifficulty",
+        evidenceStatement: "evidenceStatement"
+    }
+
+    const item: ApiModels.Resource<AboutItemVM.AboutThisItem> = {
+        kind: "none",
+        content
+    }
+
     const props: Props = {
-        onRowSelection: jest.fn((item: {itemKey: number; bankKey: number}) => {return null}),
-        itemCards 
+        onRowSelection: jest.fn((item: { itemKey: number; bankKey: number }) => { return null }),
+        itemCards,
+        item
     }
 
     it("matches snapshot", () => {
-        expect(shallow(<ItemPageTable {...props}/>)).toMatchSnapshot();;
+        expect(shallow(<ItemPageTable {...props} />)).toMatchSnapshot();;
     })
 
     it("sorts list on header click", () => {
-        let wrapper = mount(<ItemPageTable {...props}/>);
+        let wrapper = mount(<ItemPageTable {...props} />);
         tabs.forEach(tab => {
             const className = `th.${tab}`;
             wrapper.find(className).simulate('click');
@@ -47,8 +68,8 @@ describe("ItemPageTable", () => {
         })
     })
 
-    it("calls onRowSelection()", ()  => {
-        let wrapper = mount(<ItemPageTable {...props}/>);
+    it("calls onRowSelection()", () => {
+        let wrapper = mount(<ItemPageTable {...props} />);
         wrapper.find('td.item').simulate('click');
         expect(props.onRowSelection).toHaveBeenCalled();
     })
--- ItemTable/__tests__/ItemTable.test.tsx
+++ ItemTable/__tests__/ItemTable.test.tsx
@@ -2,9 +2,47 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { DataTable } from '../ItemTable';
 import * as TestUtils from 'react-dom/test-utils';
+import * as ItemCardViewModel from '../../Models/ItemCardViewModel';
+import * as GradeLevels from '../../Models/GradeLevels';
+import * as Rubric from '../../PageTabs/Rubric';
+import * as AboutItemVM from '../../Models/AboutItemVM';
+import * as ApiModels from '../../Models/ApiModels';
 import { shallow, mount } from 'enzyme';
 
 describe("ItemTable", () => {
+    const itemCards: ItemCardViewModel.ItemCardViewModel[] = [{
+        bankKey: 1,
+        itemKey: 3,
+        title: "",
+        grade: GradeLevels.GradeLevels.All,
+        gradeLabel: "",
+        subjectCode: "",
+        subjectLabel: "",
+        claimCode: "",
+        claimLabel: "",
+        target: "",
+        interactionTypeCode: "",
+        interactionTypeLabel: "",
+        isPerformanceItem: true
+    }]
+
+    const rubrics: Rubric.Rubric[] = []
+
+    const content: AboutItemVM.AboutThisItem = {
+        rubrics,
+        itemCardViewModel: itemCards[0],
+        depthOfKnowledge: "depthOfKnowledge",
+        targetDescription: "targetDescription",
+        commonCoreStandardsDescription: "commonCoreStandardsDescription",
+        educationalDifficulty: "educationalDifficulty",
+        evidenceStatement: "evidenceStatement"
+    }
+
+    const item: ApiModels.Resource<AboutItemVM.AboutThisItem> = {
+        kind: "none",
+        content
+    }
+    
     const props = {
         tableRef: jest.fn(),
         mapRows: [],
@@ -25,7 +63,8 @@ describe("ItemTable", () => {
                 interactionTypeCode: "type01",
                 interactionTypeLabel: "type01label",
                 isPerformanceItem: false
-        }
+        },
+        item
     }
     
     it("matches snapshot", () => {
