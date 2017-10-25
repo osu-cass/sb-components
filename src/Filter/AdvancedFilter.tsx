617a04ae3c10b85376ba89b56a8746e6eec73d24
Van Horn <vanhornd@oregonstate.edu>
2017-10-25 09:33:41 -0700

    The references for font-awesone are only needed for testing

--- src/Filter/AdvancedFilter.tsx
+++ src/Filter/AdvancedFilter.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { AdvancedFilterCategory, AdvancedFilterOption, OptionType } from './AdvancedFilterModel';
 import "../styles/AdvancedFilter.css";
-import "font-awesome/css/font-awesome.min.css";
 
 export interface Props extends AdvancedFilterCategory { 
     selectedHandler: (data?: AdvancedFilterOption) => void;
