41cdc99508939d82a34f8242091a84b39fc7b92e
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2017-11-02 15:09:22 -0700

    fixed passage headers to display all like questions

--- PassageView.tsx
+++ PassageView.tsx
@@ -4,15 +4,14 @@ import { ItemViewComponent } from './ItemView';
 
 interface Props {
     view: ItemView;
-    associatedItems: string;
+    associatedItems: string[];
 }
 
 export class PassageView extends React.Component<Props, {}> {
     render() {
-        const items = this.props.associatedItems.split(',');
         return (
             <div className='item'>
-                <h2>Passage for {items.length === 1 ? 'item' : 'items'} {items.join(', ')}</h2>
+                <h2>Passage for {this.props.associatedItems.length === 1 ? 'item' : 'items'} {this.props.associatedItems.join(', ')}</h2>
                 <ItemViewComponent view={this.props.view} />
             </div>
         );
