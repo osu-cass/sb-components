a432477d4b93e60f63066294a70602a7f4bd0f3d
Rob Mac <robmac@maccelerated.me>
2017-11-03 16:43:00 -0700

    Refactor a lot of stuff
    Move config into .env file; move some static assets into server project.
    
    GradeLevels.ts is duplicate code and will need to be moved.

--- index.tsx
+++ index.tsx
@@ -3,14 +3,15 @@ import * as ReactDOM from 'react-dom';
 import { ScoringGuidePage, ItemsSearchViewModel } from './ScoreGuide/ScoringGuidePage';
 import { get } from "./Models/ApiModels";
 import "font-awesome/css/font-awesome.min.css";
+import './styles/site.less'
 
 export class App extends React.Component<{},{}>{
     client = () => get<ItemsSearchViewModel>("http://is-score.cass.oregonstate.edu/ScoringGuide/ScoringGuideViewModel");
-    
+
     render() {
         return (<ScoringGuidePage scoreGuideViewModelClient={this.client}/>);
     }
-    
+
 }
 
 ReactDOM.render(<App/>, document.getElementById("react-container"));
