7c89b08d18d65d42d9539e90335d026f91b0fde8
Daniel Van Horn <dan.vanhorn17@gmail.com>
2017-11-15 10:29:36 -0800

    Remove unfinished test from future commit

--- stories/ItemPage/ItemPage.storybook.tsx
+++
@@ -1,6 +0,0 @@
-import { ItemPage } from "../../src/ItemPage/ItemPage";
-import { storiesOf } from "@storybook/react";
-import { CenterDecorator } from "../CenterDecorator";
-
-// storiesOf("ItemPage", module)
-// .add("", () => {<ItemPage />});
--- stories/ItemPage/ItemPageContainer.storybook.tsx
+++
@@ -1,5 +0,0 @@
-import { ItemPageContainer } from "../../src/ItemPage/ItemPageContainer";
-import { storiesOf } from "@storybook/react";
-import { CenterDecorator } from "../CenterDecorator";
-
-// storiesOf("ItemPageContainer", module)
