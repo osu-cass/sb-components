d4fe31f688669d2c3481aecf0687377cdee06366
Daniel Van Horn <dan.vanhorn17@gmail.com>
2017-11-16 13:01:24 -0800

    tests do not run

--- src/ItemViewer/__tests__/ItemViewerFrame.test.tsx
+++ src/ItemViewer/__tests__/ItemViewerFrame.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { ItemFrame } from '../ItemViewerFrame';
+import { ItemViewerFrame } from '../ItemViewerFrame';
 import * as TestUtils from 'react-dom/test-utils';
 import { shallow, mount, render } from 'enzyme';
 
@@ -10,18 +10,18 @@ describe("ItemViewerFrame", () => {
     }
 
     it("matches snapshot while loading", () => {
-        let wrapper = shallow(<ItemFrame {...obj}/>);
+        let wrapper = shallow(<ItemViewerFrame {...obj}/>);
         wrapper.setState({loading: true});
         expect(wrapper).toMatchSnapshot();
     })
 
     it("after page content is loaded", () => {
-        let wrapper = shallow(<ItemFrame {...obj}/>);
+        let wrapper = shallow(<ItemViewerFrame {...obj}/>);
         wrapper.setState({loading: false});
         expect(wrapper).toMatchSnapshot();
     })
 
     it("no item found", () => {
-        expect(shallow(<ItemFrame url=""/>)).toMatchSnapshot();
+        expect(shallow(<ItemViewerFrame url=""/>)).toMatchSnapshot();
     })
 })
