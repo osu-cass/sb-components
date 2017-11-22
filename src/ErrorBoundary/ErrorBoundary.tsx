41f9e3850bfeeedd7d939c25c3bb8a454c6ba00c
Daniel Van Horn <dan.vanhorn17@gmail.com>
2017-11-22 13:54:15 -0800

    create ErrorBoundary component

---
+++ src/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+
+export interface ErrorBoundaryProps {}
+
+export interface ErrorBoundaryState {
+    hasError: boolean,
+    error?: Error
+}
+
+export class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState>{
+    constructor ( props: ErrorBoundaryProps ) {
+        super( props );
+        this.state = { hasError: false };
+    }
+
+    componentDidCatch (error: Error, info: any) {
+        // Display fallback UI
+        this.setState( { hasError: true } );
+        // You can also log the error to an error reporting service
+        // logError(error);
+    }
+
+    render () {
+        const {hasError, error} = this.state;
+        let content = this.props.children;
+        if ( hasError ) {
+            // You can render any custom fallback UI
+            content = (<div>{`An error ocurred: ${error}`}</div>);
+        }
+        return content;
+    }
+}
