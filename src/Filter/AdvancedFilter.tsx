3f71a40d5aa4c2758513fba94674758d78017fc8
Daniel Van Horn <dan.vanhorn17@gmail.com>
2017-11-24 11:39:17 -0800

    disabled tags are now unclickable & greyed out but visible

--- src/Filter/AdvancedFilter.tsx
+++ src/Filter/AdvancedFilter.tsx
@@ -7,20 +7,20 @@ import {
 } from "./AdvancedFilterModel";
 
 export interface AdvancedFilterProps extends AdvancedFilterCategoryModel {
-  selectedHandler: (data?: FilterOptionModel) => void;
+  selectedHandler: ( data?: FilterOptionModel ) => void;
 }
 
 export class AdvancedFilter extends React.Component<AdvancedFilterProps, {}> {
-  constructor(props: AdvancedFilterProps) {
-    super(props);
+  constructor ( props: AdvancedFilterProps ) {
+    super( props );
   }
 
-  renderAllbtnContainer() {
+  renderAllbtnContainer () {
     let allBtnContainer: JSX.Element | undefined;
     let classname = "";
-    const anySelected = this.props.filterOptions.some(fo => fo.isSelected);
+    const anySelected = this.props.filterOptions.some( fo => fo.isSelected );
 
-    if (this.props.displayAllButton) {
+    if ( this.props.displayAllButton ) {
       classname = anySelected ? "" : " selected-button";
 
       allBtnContainer = (
@@ -38,31 +38,38 @@ export class AdvancedFilter extends React.Component<AdvancedFilterProps, {}> {
     return allBtnContainer;
   }
 
-  renderTags() {
+  renderTags () {
     const tags: JSX.Element[] = [];
     let classname = "";
-    if (this.props.filterOptions) {
-      this.props.filterOptions.forEach((t, i) => {
+    if ( this.props.filterOptions ) {
+      this.props.filterOptions.forEach( ( t, i ) => {
         classname = t.isSelected ? "selected-button" : "";
-
-        tags.push(
-          <button
-            className={classname + " filter-button"}
-            key={t.key}
-            onClick={() => this.props.selectedHandler(t)}
-          >
-            {t.label}
-          </button>
-        );
-      });
+        if ( !this.props.disabled ) {
+          tags.push(
+            <button
+              className={classname + " filter-button"}
+              key={t.key}
+              onClick={() => this.props.selectedHandler( t )}
+            >
+              {t.label}
+            </button>
+          );
+        } else {
+          tags.push(
+            <button className="disabled-filter-button" key={t.key}>
+              {t.label}
+            </button>
+          )
+        }
+      } );
     }
     return tags;
   }
 
-  render() {
+  render () {
     return (
       <div
-        id={(this.props.label + "-filter").toLocaleLowerCase()}
+        id={( this.props.label + "-filter" ).toLocaleLowerCase()}
         className="filter-selection"
       >
         <label>
