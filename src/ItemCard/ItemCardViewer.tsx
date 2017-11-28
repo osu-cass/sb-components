f5245f319e6b0334a13bdb7d05a42cb4a171cdff
Daniel Van Horn <dan.vanhorn17@gmail.com>
2017-11-28 11:23:58 -0800

    test

--- src/ItemCard/ItemCardViewer.tsx
+++ src/ItemCard/ItemCardViewer.tsx
@@ -31,6 +31,7 @@ export class ItemCardViewer extends React.Component<
   renderViewer(url: string) {
     return (
       <div className="item-content">
+      
         <ItemViewerFrame url={url} />
       </div>
     );
