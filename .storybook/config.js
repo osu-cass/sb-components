7facd4834cc32cac24319620585589463f7df40c
Alex Lepinski <alexjlepinski@gmail.com>
2017-12-02 00:33:08 -0800

    removed enzyme adapter for storybook testing to resolve issue with storybook output

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,10 +5,6 @@ import '../src/Styles/site.less';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../src/Styles/custom.less';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 const req = require.context("../stories", true, /\.storybook\.tsx$/);
 addDecorator((storyFn, context) => withConsole()(storyFn)(context));
