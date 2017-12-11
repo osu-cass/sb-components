dabc39865a42aff2afcba08afbdc4e04b70a0ba0
Kevin Turkington <turkingtonkevin@gmail.com>
2017-12-11 15:05:32 -0800

    selectable mock for item table container

--- stories/ItemTable/ItemTableContainer.storybook.tsx
+++ stories/ItemTable/ItemTableContainer.storybook.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import { CenterDecorator } from "../CenterDecorator";
-import { itemTableProps, itemTableSortProps } from "./Mocks";
+import {
+  itemTableProps,
+  itemTableSortProps,
+  itemTableSelectProps
+} from "./Mocks";
 import { ItemTableContainer } from "../../src/ItemTable/ItemTableContainer";
 
 storiesOf("Item Table Container", module)
@@ -9,4 +13,5 @@ storiesOf("Item Table Container", module)
   .add("normal render", () => <ItemTableContainer {...itemTableProps} />)
   .add("verify sort order", () => (
     <ItemTableContainer {...itemTableSortProps} />
-  ));
+  ))
+  .add("selected item", () => <ItemTableContainer {...itemTableSelectProps} />);
