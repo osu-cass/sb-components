74abac72b10ab388a4914b96d45d183f9df5e0aa
Daniel Van Horn <dan.vanhorn17@gmail.com>
2017-12-11 17:59:52 -0800

    add tsdoc headers

--- src/Filter/FilterLink.tsx
+++ src/Filter/FilterLink.tsx
@@ -1,9 +1,22 @@
 import "../Assets/Styles/filter-link.less";
 import * as React from "react";
 
+/**
+ * The filterId is an element id that refers to the id of the Filter
+ * element on the DOM
+ * @interface FilterLinkProps
+ * @member {string} filterId
+ */
 export interface FilterLinkProps {
   filterId: string;
 }
+
+/**
+ * A link button that is fixed to the bottom of the screen that navigates to the
+ * filter on the DOM.
+ * @class FilterLink
+ * @extends {React.Component<FilterLinkProps, {}>}
+ */
 export class FilterLink extends React.Component<FilterLinkProps, {}> {
   render() {
     return (
