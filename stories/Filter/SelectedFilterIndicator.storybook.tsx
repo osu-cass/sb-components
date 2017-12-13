e486da4e3f1f1742c35fc8665880581bc7b331a5
Daniel Van Horn <vanhornd@oregonstate.edu>
2017-12-12 17:25:53 -0800

    Add tests for SelectedFilterIndicator

--- stories/Filter/SelectedFilterIndicator.storybook.tsx
+++ stories/Filter/SelectedFilterIndicator.storybook.tsx
@@ -1,2 +1,15 @@
 import * as React from "react";
+import { SelectedFilterIndicator } from "../../src/Filter/SelectedFilterIndicator";
+import { mockAdvancedFilterCategoriesSelected } from "./mocks";
+import { CenterDecorator } from "../CenterDecorator";
 import { storiesOf } from "@storybook/react";
+
+storiesOf("SelectedFilterIndicator", module)
+  .addDecorator(CenterDecorator)
+  .add("selected filter", () => (
+    <SelectedFilterIndicator
+      category={mockAdvancedFilterCategoriesSelected[0]}
+      option={mockAdvancedFilterCategoriesSelected[0].filterOptions[0]}
+      onClick={() => {}}
+    />
+  ));
