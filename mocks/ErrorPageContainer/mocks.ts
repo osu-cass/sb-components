269f900e0832882c3059047e14d4b5000e943635
Kevin Turkington <turkingtonkevin@gmail.com>
2017-12-22 09:48:16 -0800

    fixing storybook

--- mocks/ErrorPageContainer/mocks.ts
+++ mocks/ErrorPageContainer/mocks.ts
@@ -1,13 +1,13 @@
 import { pageType, ErrorPageContainerProps } from "src/ErrorPageContainer/ErrorPageContainer";
 
 export const serverErrMock: ErrorPageContainerProps = {
-  pageRender: pageType.ServerError,
+  errorCode: pageType.ServerError,
   errorMsg: "you did a bad thing that caused errors",
   description: "stop doing that thing that caused errors"
 };
 
 export const notFoundErrMock: ErrorPageContainerProps = {
-  pageRender: pageType.NotFound,
+  errorCode: pageType.NotFound,
   errorMsg: "This item cannot be found",
   description: "Please try another search"
 };
--- stories/ErrorPageContainer/ErrorPageContainer.storybook.tsx
+++ stories/ErrorPageContainer/ErrorPageContainer.storybook.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import { ItemTableContainer } from "../../src/index";
 import { ErrorPageContainer } from "../../src/ErrorPageContainer/ErrorPageContainer";
-import { serverErrMock, notFoundErrMock } from "./mocks";
+import { serverErrMock, notFoundErrMock } from "mocks/ErrorPageContainer/mocks";
 
 storiesOf("Error Page Container", module)
   .add("empty props render", () => <ErrorPageContainer />)
