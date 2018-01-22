a2b6e0430cb9a91abcac59831b410c3537a311cd
millcour <courtney.miller005@gmail.com>
2018-01-22 12:42:30 -0800

    Added components to wireframe and started less file for styling

--- src/ItemBank/ItemBank.tsx
+++ src/ItemBank/ItemBank.tsx
@@ -1,7 +1,99 @@
+import {
+  AboutItem,
+  AboutItemModel,
+  getResourceContent,
+  ItemAccessibilityModal,
+  ItemViewerFrame
+} from "../index";
 import * as React from "react";
+import { AboutItemMockModel } from "mocks/AboutItem/mocks";
+import { AccResourceGroupModel } from "../index";
 
-export class ItemBank extends React.Component<{}, {}> {
+export interface ItemBankState {
+  itemUrl?: string;
+  aboutThisItemViewModel: AboutItemModel;
+  accResourceGroups: AccResourceGroupModel[];
+  revisions: string[];
+}
+
+export class ItemBank extends React.Component<{}, ItemBankState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      aboutThisItemViewModel: AboutItemMockModel,
+      itemUrl: "http://ivs.smarterbalanced.org/items?ids=187-3377",
+      accResourceGroups: [],
+      revisions: ["sample", "revisions", "for", "styling"]
+    };
+  }
+
+  renderItemFrame(): JSX.Element {
+    const aboutThisItem = this.state.aboutThisItemViewModel;
+    let content: JSX.Element;
+
+    if (aboutThisItem) {
+      content = (
+        <div
+          className="about-item-iframe"
+          aria-live="polite"
+          aria-relevant="additions removals"
+        >
+          <div
+            className="item-nav"
+            role="toolbar"
+            aria-label="Toolbar with button groups"
+          >
+            <div
+              className="item-nav-left-group"
+              role="group"
+              aria-label="First group"
+            >
+              <AboutItem showRubrics={false} {...aboutThisItem} />
+            </div>
+            {this.renderRightNav()}
+            {this.renderRevisionsContainer()}
+          </div>
+          <ItemViewerFrame url={this.state.itemUrl || ""} />
+        </div>
+      );
+    } else {
+      content = <div />;
+    }
+
+    return content;
+  }
+
+  renderRightNav(): JSX.Element {
+    return (
+      <div
+        className="item-nav-right-group"
+        role="group"
+        aria-label="Second group"
+      >
+        <ItemAccessibilityModal
+          accResourceGroups={this.state.accResourceGroups}
+          onSave={() => {}}
+          onReset={() => {}}
+        />
+      </div>
+    );
+  }
+
+  renderRevisions = (rev: string) => {
+    const revision = rev;
+    return (
+      <ul>
+        <li>{revision}</li>
+      </ul>
+    );
+  };
+
+  renderRevisionsContainer() {
+    const revisions = this.state.revisions.map(this.renderRevisions);
+    return <div className="revisions">{revisions}</div>;
+  }
+
   render() {
-    return <div />;
+    return <div className="container">{this.renderItemFrame()}</div>;
   }
 }
--- stories/ItemBank/ItemBank.storybook.tsx
+++ stories/ItemBank/ItemBank.storybook.tsx
@@ -1,9 +1,19 @@
+import { Layout } from "../../src/index";
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import { ItemBank } from "src/ItemBank/ItemBank";
+import { AboutItem } from "src/AboutItem/AboutItem";
 import { AboutItemMockModel } from "mocks/AboutItem/mocks";
 import { CenterDecorator } from "../CenterDecorator";
+import { ItemAccessibilityModal } from "src/";
+import { RouterDecorator } from "../RouterDecorator";
+import {
+  accessibilityModalProp,
+  mockAccResourceGroups
+} from "mocks/Accessibility/mocks";
 
 storiesOf("Item Bank", module)
-  .addDecorator(CenterDecorator)
-  .add("default", () => <ItemBank />);
+  .addDecorator(RouterDecorator)
+  .add("default", () => (
+    <Layout children={<ItemBank />} siteName="Item Bank" />
+  ));
