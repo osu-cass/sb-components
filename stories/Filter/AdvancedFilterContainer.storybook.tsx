6a10f3b147027a057b680f05c11cbd9c6b2ef0a9
Alex Lepinski <alexjlepinski@gmail.com>
2018-01-30 22:50:34 -0800

    updated project packages for jest, storybook, and typescript, changed a11y decorator to be global and fixes issue with Node undefined during test, story configuration changes

--- stories/Filter/AdvancedFilterContainer.storybook.tsx
+++ stories/Filter/AdvancedFilterContainer.storybook.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-import { checkA11y } from "@storybook/addon-a11y";
 import {
   AdvFilContainerTestWrapper,
   AdvFilContainerTestProps
@@ -12,6 +11,5 @@ const props: AdvFilContainerTestProps = {
 };
 
 storiesOf("Advanced Filter Container", module)
-  .addDecorator(checkA11y)
   .add("normal render", () => <AdvFilContainerTestWrapper />)
   .add("with page title", () => <AdvFilContainerTestWrapper {...props} />);
