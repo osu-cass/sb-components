91fc6d7ac6e1d19d0ea7d37533b8310472ed0f3d
millcour <courtney.miller005@gmail.com>
2018-01-31 16:04:36 -0800

    Changed the styling for tool tip and made the component more generic

--- src/ToolTip/ToolTip.tsx
+++ src/ToolTip/ToolTip.tsx
@@ -1,65 +1,60 @@
 import * as React from "react";
 
 export interface ToolTipProps {
+  toolTipHeader?: string;
   helpText?: string;
   displayIcon?: boolean;
   position?: "top" | "bottom";
 }
 
-export interface ToolTipState {
-  hovered: boolean;
-  focused: boolean;
-}
-
-export class ToolTip extends React.Component<ToolTipProps, ToolTipState> {
+export class ToolTip extends React.Component<ToolTipProps, {}> {
   constructor(props: ToolTipProps) {
     super(props);
-
-    this.state = {
-      hovered: false,
-      focused: false
-    };
   }
 
-  onHover = () => this.setState({ hovered: true });
-  offHover = () => this.setState({ hovered: false });
-  onFocus = () => this.setState({ focused: true });
-  offFocus = () => this.setState({ focused: false });
-
-  render() {
-    if (!this.props.helpText) {
-      return <span> {this.props.children}</span>;
+  renderToolTipHeader() {
+    if (this.props.toolTipHeader) {
+      return (
+        <div className="tool-tip-header">
+          <h3 className="tool-tip-link-header">{this.props.toolTipHeader}</h3>
+        </div>
+      );
     }
+  }
 
-    const position = this.props.position || "top";
-    const shown = this.state.focused || this.state.hovered;
-
+  renderToolTipVisibleText() {
     const icon = this.props.displayIcon ? (
       <span className="fa fa-info-circle fa-sm" />
-    ) : null;
-
-    const toolTipContent = shown ? (
-      <div className={"tool-tip-before " + position}>{this.props.helpText}</div>
-    ) : null;
-    const toolTipArrow = shown ? (
-      <div className={"tool-tip-after " + position} />
-    ) : null;
+    ) : (
+      undefined
+    );
 
     return (
-      <span className={"tool-tip " + position}>
-        {toolTipContent}
-        <span
-          className="tool-tip-hoverable"
-          onMouseEnter={this.onHover}
-          onMouseLeave={this.offHover}
-          onFocus={this.onFocus}
-          onBlur={this.offFocus}
-          tabIndex={0}
-        >
-          {this.props.children} {icon}
-        </span>
-        {toolTipArrow}
-      </span>
+      <div className="tool-tip-hoverable" tabIndex={0}>
+        {this.props.children} {icon}
+      </div>
     );
+  }
+
+  renderToolTipHelpText() {
+    return (
+      <div className="tool-tip-message">
+        {this.renderToolTipHeader()}
+        <div className="tool-tip-commit-message">{this.props.helpText}</div>
+      </div>
+    );
+  }
+
+  renderToolTip() {
+    return (
+      <div className="tool-tip-links">
+        {this.renderToolTipVisibleText()}
+        <div className="tool-tip-details">{this.renderToolTipHelpText()}</div>
+      </div>
+    );
+  }
+
+  render() {
+    return <div>{this.renderToolTip()}</div>;
   }
 }
--- stories/ToolTip/ToolTip.storybook.tsx
+++ stories/ToolTip/ToolTip.storybook.tsx
@@ -3,9 +3,20 @@ import { storiesOf } from "@storybook/react";
 import { ToolTip } from "src/ToolTip/ToolTip";
 import { CenterDecorator } from "../CenterDecorator";
 
+const longText =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ";
+const header = "Header for my tool tip";
 storiesOf("ToolTip", module)
   .addDecorator(CenterDecorator)
   .add("with just text", () => <ToolTip helpText="test">Testing</ToolTip>)
+  .add("with just long text", () => (
+    <ToolTip helpText={longText}>Testing</ToolTip>
+  ))
+  .add("with just text and header", () => (
+    <ToolTip helpText={longText} toolTipHeader={header}>
+      Testing
+    </ToolTip>
+  ))
   .add("with icon", () => (
     <ToolTip helpText="test" displayIcon={true}>
       Testing
