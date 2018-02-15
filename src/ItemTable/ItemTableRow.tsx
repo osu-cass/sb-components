66437784ee2c98bd2a53d7051da88e44f178751b
Kevin Turkington <turkingtonkevin@gmail.com>
2018-02-15 10:11:12 -0800

    migrating item table css to another file and condensing css.

--- src/ItemTable/ItemTableRow.tsx
+++ src/ItemTable/ItemTableRow.tsx
@@ -112,6 +112,7 @@ export class ItemTableRow extends React.Component<ItemTableRowProps, {}> {
     if (hasControls) {
       controls = [
         <td
+          className="item-checkbox"
           key={`checkbox-control`}
           onClick={e => this.handleCheckboxClick(e, rowData)}
           onKeyUp={e => this.handleCheckboxKeyUpEnter(e, rowData)}
@@ -119,7 +120,7 @@ export class ItemTableRow extends React.Component<ItemTableRowProps, {}> {
         >
           {rowData.selected === true ? checked : unChecked}&nbsp;
         </td>,
-        <td tabIndex={0} key={`expand-control`}>
+        <td className="arrow-indicator" tabIndex={0} key={`expand-control`}>
           {isExpanded ? expand : collapse}
         </td>
       ];
