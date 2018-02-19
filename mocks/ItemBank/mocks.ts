bfe12c3cb54527cb3e69cec967142e5a5849b05e
alexlepinski <alex.lepinski@oregonstate.edu>
2018-02-18 23:48:19 -0800

    added constant date to fix snapshot tests, mocks husky prettier

--- mocks/ItemBank/mocks.ts
+++ mocks/ItemBank/mocks.ts
@@ -1,30 +1,33 @@
-import * as ItemBank from "src/ItemBank/ItemBank"
+import * as ItemBank from "src/ItemBank/ItemBank";
+
+const testDate = new Date("11/11/2015");
 
 export const RevisionMockModelOne: ItemBank.RevisionModel = {
-    author: "Troy Barnes",
-    date: new Date(),
-    commitMessage: "Added functionality to the website",
-    commitHash: "ab65jg"
-}
+  author: "Troy Barnes",
+  date: testDate,
+  commitMessage: "Added functionality to the website",
+  commitHash: "ab65jg"
+};
 
 export const RevisionMockModelTwo: ItemBank.RevisionModel = {
-    author: "Pierce Hawthorne",
-    date: new Date(),
-    commitMessage: "I want to see what happens when there is a much longer commit message than all of the rest",
-    commitHash: "h4lso6"
-}
+  author: "Pierce Hawthorne",
+  date: testDate,
+  commitMessage:
+    "I want to see what happens when there is a much longer commit message than all of the rest",
+  commitHash: "h4lso6"
+};
 
 export const RevisionMockModelThree: ItemBank.RevisionModel = {
-    author: "Annie Edison",
-    date: new Date(),
-    commitMessage: "Changed one of the pages",
-    commitHash: "k5ls58"
-}
+  author: "Annie Edison",
+  date: testDate,
+  commitMessage: "Changed one of the pages",
+  commitHash: "k5ls58"
+};
 
 export const ItemBankPropsMockModel: ItemBank.ItemBankProps = {
-    revisions: [
-        RevisionMockModelOne,
-        RevisionMockModelTwo,
-        RevisionMockModelThree
-    ]
-}
+  revisions: [
+    RevisionMockModelOne,
+    RevisionMockModelTwo,
+    RevisionMockModelThree
+  ]
+};
