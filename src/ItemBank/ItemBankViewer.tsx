a0d8ea8d53a65d2cb955d5dd951e6b7a8f8cd6af
millcour <courtney.miller005@gmail.com>
2018-03-02 11:07:28 -0800

    Separated logic for rendering navigation buttons, and styled the buttons'

--- src/ItemBank/ItemBankViewer.tsx
+++ src/ItemBank/ItemBankViewer.tsx
@@ -64,29 +64,51 @@ export class ItemBankViewer extends React.Component<ItemBankViewerProps, {}> {
     return content;
   }
 
+  renderNavButton(direction: "next" | "previous") {
+    const { onItemSelect, nextItem, prevItem } = this.props;
+    let label = "";
+    let ItemName = "";
+    let item = nextItem;
+    if (direction == "previous") {
+      ItemName = prevItem ? getItemBankName(prevItem) : " ";
+      label = "Previous";
+      item = prevItem;
+    } else {
+      ItemName = nextItem ? getItemBankName(nextItem) : " ";
+      label = "Next";
+    }
+
+    return (
+      <button
+        onClick={() => onItemSelect(direction)}
+        className={"btn btn-link " + direction}
+        disabled={item ? false : true}
+      >
+        <div className="nav-button-container">
+          <span
+            className={
+              direction == "previous" ? "fa fa-arrow-left" : "fa fa-arrow-right"
+            }
+            aria-hidden="true"
+          />
+          <div className="nav-item-link">
+            <div>{label}</div>
+            <div className="nav-item-name">{ItemName}</div>
+          </div>
+        </div>
+      </button>
+    );
+  }
+
   renderMidNav() {
     const { onItemSelect, nextItem, prevItem } = this.props;
-    const nextItemName = nextItem ? getItemBankName(nextItem) : "";
-    const previousItemName = prevItem ? getItemBankName(prevItem) : "";
+    const nextItemName = nextItem ? getItemBankName(nextItem) : " ";
+    const previousItemName = prevItem ? getItemBankName(prevItem) : " ";
 
     return (
       <div className="nav-buttons">
-        <button
-          onClick={() => onItemSelect("previous")}
-          className="btn btn-link previous"
-          disabled={prevItem ? false : true}
-        >
-          <span className="fa fa-arrow-left" aria-hidden="true" />
-          {`Previous ${previousItemName}`}
-        </button>
-        <button
-          onClick={() => onItemSelect("next")}
-          className="btn btn-link next"
-          disabled={nextItem ? false : true}
-        >
-          {`Next ${nextItemName}`}
-          <span className="fa fa-arrow-right" aria-hidden="true" />
-        </button>
+        {this.renderNavButton("previous")}
+        {this.renderNavButton("next")}
       </div>
     );
   }
