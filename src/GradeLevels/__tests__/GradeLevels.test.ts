d1df29c1319e97f405c7d4fa64cb24e72d2d7435
millcour <courtney.miller005@gmail.com>
2018-03-13 13:30:18 -0700

    Added testing for GradeLevels

---
+++ src/GradeLevels/__tests__/GradeLevels.test.ts
@@ -0,0 +1,70 @@
+import * as React from "react";
+import { shallow } from "enzyme";
+import { GradeLevel, GradeLevels } from "../GradeLevels";
+
+const gradeLevel6: GradeLevels = GradeLevels.Grade6;
+const gradeLevel7: GradeLevels = GradeLevels.Grade7;
+const gradeLevel8: GradeLevels = GradeLevels.Grade8;
+const gradeLevel9: GradeLevels = GradeLevels.Grade9;
+const gradeLevel10: GradeLevels = GradeLevels.Grade10;
+const gradeLevel11: GradeLevels = GradeLevels.Grade11;
+const gradeLevel12: GradeLevels = GradeLevels.Grade12;
+const gradeLevelNA: GradeLevels = GradeLevels.NA;
+const gradeLevelElementary: GradeLevels = GradeLevels.Elementary;
+const gradeLevelMiddle: GradeLevels = GradeLevels.Middle;
+const gradeLevelHigh: GradeLevels = GradeLevels.High;
+
+it("Check if grade level to string return correct value", () => {
+  const value = GradeLevel.gradeCaseToString(gradeLevel6);
+  expect(value).toEqual("Grade 6");
+});
+
+it("Check if grade level to string return correct value", () => {
+  const value = GradeLevel.gradeCaseToString(gradeLevel7);
+  expect(value).toEqual("Grade 7");
+});
+
+it("Check if grade level to string return correct value", () => {
+  const value = GradeLevel.gradeCaseToString(gradeLevel8);
+  expect(value).toEqual("Grade 8");
+});
+
+it("Check if grade level to string return correct value", () => {
+  const value = GradeLevel.gradeCaseToString(gradeLevel9);
+  expect(value).toEqual("Grade 9");
+});
+
+it("Check if grade level to string return correct value", () => {
+  const value = GradeLevel.gradeCaseToString(gradeLevel10);
+  expect(value).toEqual("Grade 10");
+});
+
+it("Check if grade level to string return correct value", () => {
+  const value = GradeLevel.gradeCaseToString(gradeLevel11);
+  expect(value).toEqual("Grade 11");
+});
+
+it("Check if grade level to string return correct value", () => {
+  const value = GradeLevel.gradeCaseToString(gradeLevel12);
+  expect(value).toEqual("Grade 12");
+});
+
+it("Check if grade level to string return correct value", () => {
+  const value = GradeLevel.gradeCaseToString(gradeLevelNA);
+  expect(value).toEqual("NA");
+});
+
+it("Check if grade level to string return correct value", () => {
+  const value = GradeLevel.gradeCaseToString(gradeLevelElementary);
+  expect(value).toEqual("Elementary");
+});
+
+it("Check if grade level to string return correct value", () => {
+  const value = GradeLevel.gradeCaseToString(gradeLevelMiddle);
+  expect(value).toEqual("Middle");
+});
+
+it("Check if grade level to string return correct value", () => {
+  const value = GradeLevel.gradeCaseToString(gradeLevelHigh);
+  expect(value).toEqual("High");
+});
