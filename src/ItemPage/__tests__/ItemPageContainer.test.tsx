726930d339691b6cd640bce591e5b6e9c7b37b39
Noelcke <noelcket@oregonstate.edu>
2018-05-09 12:00:51 -0700

    Working on tests

---
+++ src/ItemPage/__tests__/ItemPageContainer.test.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { shallow, mount, render } from "enzyme";
+import { ItemPageContainer } from "@src/index";
+import {
+  itemPageMockProps,
+  itemPageMockPropsNoItem,
+  itemPageModelMock
+} from "@mocks/ItemPage/mocks";
+
+describe("ItemPageContainer", () => {
+  console.log("Hello world");
+});
